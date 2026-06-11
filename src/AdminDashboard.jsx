25c1498056e85b62b40df3395998f6cf22a9e98a
lorenzocona14-cmyk <lorenzocona14@gmail.com>
2026-06-11 20:05:05 -0300

    AdminDashboard.jsx

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -176,7 +176,7 @@ const AdminDashboard = () => {
       if (imageFile) {
         const formData = new FormData();
         formData.append('image', imageFile);
-        const apiKey = 'TU_API_KEY_DE_IMGBB_ACA'; 
+        const apiKey = '9d8cc5a9d9abff0bc9050f7288a07f80'; 
         const imgbbRes = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}`, { method: 'POST', body: formData });
         const imgbbData = await imgbbRes.json();
         if (imgbbData.success) imageUrl = imgbbData.data.url;
